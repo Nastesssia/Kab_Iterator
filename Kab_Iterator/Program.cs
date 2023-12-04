d892c58092755f9fc12fe63a0fc92f1df0fb3e9c
Diana <fun_and_net@mail.ru>
2023-12-04 09:35:04 +0200

    Добавьте файлы проекта.

---
+++ Kab_Iterator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Kab_Iterator
+{
+    public class AlphabetLibrary : IEnumerable<char>
+    {
+        private char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+        public IEnumerator<char> GetEnumerator()
+        {
+            return ((IEnumerable<char>)alphabet).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            AlphabetLibrary library = new AlphabetLibrary();
+
+            //foreach для перебора элементов коллекции через итератор
+            foreach (char letter in library)
+            {
+                Console.WriteLine(letter); 
+            }
+            Console.ReadKey();
+        }
+        
+    }
+
+}
